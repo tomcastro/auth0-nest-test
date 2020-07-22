b4632f46bb7fb46466cc4a72a92d6bd488ad94aa
Tomás Castro <tcastrog10@gmail.com>
2020-07-22 19:57:22 -0400

    auth0 test

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common'
+import { ItemsModule } from './items/items.module'
+import { AuthzModule } from './authz/authz.module';
+
+@Module({
+  imports: [ItemsModule, AuthzModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
